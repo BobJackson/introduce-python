30ca34af13ddfa87d5cc5d098bebe3f1cfb1bdd2
Bob Jackson <wangyousong@9191offer.com>
2024-03-11 13:00:49 +0800

    add related files for ch19

---
+++ style3.py
@@ -0,0 +1,11 @@
+"Module docstring goes here"
+
+
+def func():
+    "Function docstring goes here. Hi, Mom!"
+    first = 1
+    second = 2
+    third = 3
+    print(first)
+    print(second)
+    print(third)
